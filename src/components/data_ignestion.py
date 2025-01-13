be20e85db20216b49fd8cd5258ca05b19315ff40
Syed Wasif Murtaza Jafri <wasifjafri94@gmail.com>
2025-01-13 20:36:11 +0100

    data transformation code added

--- src/components/data_ignestion.py
+++ src/components/data_ignestion.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -49,4 +50,8 @@ class DataIngestion:
         
 if __name__ == "__main__":
     data_ingestion = DataIngestion()
-    data_ingestion.initiate_data_ingestion()
+    train_data_path,test_data_path = data_ingestion.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    
+    data_transformation.initiate_data_transformation(train_data_path,test_data_path)
