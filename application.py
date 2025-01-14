60c6cc8529b410f1cabf500208f8d559d2742dc4
Syed Wasif Murtaza Jafri <wasifjafri94@gmail.com>
2025-01-14 21:13:32 +0100

    home page changes

--- application.py
+++ application.py
@@ -37,7 +37,7 @@ def predict_datapoint():
         predict_pipeline = PredictPipeline()
         result = predict_pipeline.predict(pred_df)
         
-        return render_template('home.html', result=result[0])
+        return render_template('home.html', result=(f'Predicted Math score is {result[0]}'))
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
